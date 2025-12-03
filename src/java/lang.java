6410056e27ae1b63b731c50bdec8f3c44a590276
izhank216 <izhank216@gmail.com>
2025-12-03 22:13:43 +0500

    Create lang.java

---
+++ src/java/lang.java
@@ -0,0 +1,29 @@
+package lang;
+
+import java.lang.*;
+import java.lang.reflect.*;
+
+public class JavaLangWrapper {
+
+    // Generic method to create any java.lang class instance
+    public static Object create(String className, Object... args) throws Exception {
+        Class<?> cls = Class.forName("java.lang." + className);
+        for (Constructor<?> ctor : cls.getConstructors()) {
+            if (ctor.getParameterCount() == args.length) {
+                return ctor.newInstance(args);
+            }
+        }
+        throw new RuntimeException("No matching constructor found for " + className);
+    }
+
+    // Generic method to call any method on java.lang class
+    public static Object call(Object obj, String methodName, Object... args) throws Exception {
+        Class<?> cls = obj.getClass();
+        for (Method m : cls.getMethods()) {
+            if (m.getName().equals(methodName) && m.getParameterCount() == args.length) {
+                return m.invoke(obj, args);
+            }
+        }
+        throw new RuntimeException("No matching method " + methodName + " found");
+    }
+}
